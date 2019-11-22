aa600de2a7ad5e7eb50a081e930155262bb61b0f
k832 <58082632+k832@users.noreply.github.com>
2019-11-22 11:01:37 -0500

    Add files via upload

---
+++ GerajKMadLibs.py
@@ -0,0 +1,13 @@
+singer = input("Enter a singer: ")
+subject = input("Enter a subject: ")
+hobby = input("Enter a hobby: ")
+movie_genre = input("Enter a movie_genre: ")
+food = input("Enter a food: ")
+
+
+print("My favorite singer  " + singer)
+print("is having a concert in New York about " + hobby)
+print("while watching " + movie_genre)
+print("and eating " + food)
+print("that involve " + subject)
+print("and " + food)
